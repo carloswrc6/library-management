b44a85beea27337150cc8ac2238afdc36d6664b9
Calochop :3 <50759985+carloswrc6@users.noreply.github.com>
2025-01-29 20:39:30 -0500

    fix(login): resolve authentication error

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,17 +1,20 @@
-import AuthForm from "@/components/AuthForm";
-import { signInWithCredentials } from "@/lib/actions/auth";
-import { signUpSchema } from "@/lib/validations";
+"use client";
+
 import React from "react";
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validations";
+import { signInWithCredentials } from "@/lib/actions/auth";
 
-const page = () => {
-  return (
-    <AuthForm
-      type="SIGN_IN"
-      schema={signUpSchema}
-      defaultValues={{ email: "", password: "" }}
-      onSubmit={signInWithCredentials}
-    ></AuthForm>
-  );
-};
+const Page = () => (
+  <AuthForm
+    type="SIGN_IN"
+    schema={signInSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+    }}
+    onSubmit={signInWithCredentials}
+  />
+);
 
-export default page;
+export default Page;
